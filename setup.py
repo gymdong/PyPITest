afa8498e2713721b5eac811edc4623d6e7a8d417
Bibam <61023613+gymdong@users.noreply.github.com>
2022-05-04 10:49:19 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True,
+  name='OssMyText0811',
+  version='0.0.1',
+  description='oss-dev test text output',
+  author='DongHyun',
+  author_email='gymdong@naver.com',
+  url='https://github.com/gymdong/PyPITest',
+  download_url='',
+  install_requires=['pytest'],
+  long_description='oss-dev test output upload test',
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Langauge :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
+  
